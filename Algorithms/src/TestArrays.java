2cda51f42db0facc2c08aad5e2241fddb97c2306
Dkolomy <Dkolomy@hotmail.com>
2024-03-30 23:05:03 -0400

    Final Array submit

--- Algorithms/src/TestArrays.java
+++ Algorithms/src/TestArrays.java
@@ -2,7 +2,25 @@ import myarray.DynamicArray;
 
 public class TestArrays {
     public static void main(String[] args) throws Exception {
-        DynamicArray dynamicArray = new DynamicArray();
-        System.out.println(dynamicArray.capacity);
+        DynamicArray dynamicArray = new DynamicArray(5);
+        dynamicArray.add("A");
+        dynamicArray.add("B");
+        dynamicArray.add("C");
+        dynamicArray.add("D");
+        dynamicArray.add("E");
+        dynamicArray.add("F");
+
+        dynamicArray.delete("A");
+        dynamicArray.delete("B");
+        dynamicArray.delete("C");
+
+        // dynamicArray.insert(0, "X");
+        // dynamicArray.delete("A");
+        // System.out.println(dynamicArray.search("C"));
+
+        System.out.println(dynamicArray);
+        System.out.println("size: " + dynamicArray.size);
+        System.out.println("capacity: " + dynamicArray.capacity);
+        System.out.println("empty: " + dynamicArray.isEmpty());
     }
 }
