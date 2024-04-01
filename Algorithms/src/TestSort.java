197b5275d01f1c45528f02457260eb79ad8b6268
Dkolomy <Dkolomy@hotmail.com>
2024-03-31 23:27:37 -0400

    Add resursive sorting

--- Algorithms/src/TestSort.java
+++
@@ -1,5 +0,0 @@
-public class TestSort {
-  public static void main(String[] args) {
-    System.out.println("Test Sorting");
-  }
-}
--- Algorithms/src/TestSorting.java
+++ Algorithms/src/TestSorting.java
@@ -5,10 +5,101 @@ public class TestSorting {
 
     // bubbleSort(array);
     // selectionSort(array);
-    insertionSort(array);
+    // insertionSort(array);
+    // mergeSort(array);
+    quickSort(array, 0, array.length - 1);
 
     for (int i : array) {
       System.out.print(i + " ");
+    }
+
+  }
+
+  // runtime complexity = O(n log(n))
+  // space complexity = O(log(n))
+  private static void quickSort(int[] array, int start, int end) {
+    if (end <= start)
+      return; // base case
+
+    int pivot = partition(array, start, end);
+    quickSort(array, start, pivot - 1);
+    quickSort(array, pivot + 1, end);
+
+  }
+
+  private static int partition(int[] array, int start, int end) {
+    int pivot = array[end];
+    int i = start - 1;
+
+    for (int j = start; j <= end - 1; j++) {
+      if (array[j] < pivot) {
+        i++;
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }
+    i++;
+    int temp = array[i];
+    array[i] = array[end];
+    array[end] = temp;
+
+    return i;
+  }
+
+  // runtime complexity = O(n Log n)
+  // space complexity = O(n)
+  private static void mergeSort(int[] array) {
+    int length = array.length;
+    if (length <= 1)
+      return; // base case
+
+    int middle = length / 2;
+    int[] leftArray = new int[middle];
+    int[] rightArray = new int[length - middle];
+
+    int i = 0; // leftArray
+    int j = 0; // rightArray
+
+    for (; i < length; i++) {
+      if (i < middle) {
+        leftArray[i] = array[i];
+      } else {
+        rightArray[j] = array[i];
+        j++;
+      }
+    }
+    mergeSort(leftArray);
+    mergeSort(rightArray);
+    merge(leftArray, rightArray, array);
+  }
+
+  private static void merge(int[] leftArray, int[] rightArray, int[] array) {
+    int leftSize = array.length / 2;
+    int rightSize = array.length - leftSize;
+    int i = 0, l = 0, r = 0; // indicies
+
+    // check the conditions for merging
+    while (l < leftSize && r < rightSize) {
+      if (leftArray[l] < rightArray[r]) {
+        array[i] = leftArray[l];
+        i++;
+        l++;
+      } else {
+        array[i] = rightArray[r];
+        i++;
+        r++;
+      }
+    }
+    while (l < leftSize) {
+      array[i] = leftArray[l];
+      i++;
+      l++;
+    }
+    while (r < rightSize) {
+      array[i] = rightArray[r];
+      i++;
+      r++;
     }
 
   }
